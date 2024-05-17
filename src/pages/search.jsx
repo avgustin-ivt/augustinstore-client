7e2cf7a0093ee6422c9aff692e84f24cd7c1b8d7
avgustin-ivt <78470831+avgustin-ivt@users.noreply.github.com>
2024-05-17 14:37:59 +0300

    1234

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -152,7 +152,7 @@ export default function SearchPage({ query }) {
 
   return (
     <Wrapper>
-      <SEO pageTitle="Wishlist" />
+      <SEO pageTitle="Поиск Товаров" />
       <HeaderTwo style_2={true} />
       <CommonBreadcrumb title="Поиск Товаров" subtitle="Search Products" />
       {content}
